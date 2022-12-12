e75f169c8a094d5b5d049ace8b61621785e51ad0
tuniss1 <77372423+tuniss1@users.noreply.github.com>
2022-12-12 21:21:44 +0700

    Update

--- pages/order-management/index.js
+++ pages/order-management/index.js
@@ -5,21 +5,23 @@ import { useSelector } from 'react-redux'
 import { getOrder } from 'utils/api'
 
 const OrderManagementPage = () => {
+  const userSlice = useSelector((state) => state.user)
+
   const [orderList, setOrderList] = useState([])
   const [status, setStatus] = useState(-1)
-  const userSlice = useSelector((state) => state.user)
+  console.log(userSlice)
 
   useEffect(() => {
     const fetchData = async () => {
+      if (!userSlice || !userSlice.id) return
+      console.log(status)
       const orderList = await getOrder({ userId: userSlice.id, status: status }).then(({ data }) => data.listRoom.data)
+      console.log(orderList)
 
       setOrderList(orderList)
     }
     fetchData()
-    return () => {
-      // setOrderList([])
-    }
-  }, [status])
+  }, [status, userSlice])
 
   // if (!orderList) {
   //   return <h1>Loading ...</h1>
